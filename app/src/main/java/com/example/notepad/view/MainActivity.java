61e0b56772f4b4f0f4418603e7a92d59e7bed49c
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-05-12 20:48:59 -0300

    Swipe pra deletar itens do RecyclerView implementado

--- app/src/main/java/com/example/notepad/view/MainActivity.java
+++ app/src/main/java/com/example/notepad/view/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.notepad.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,7 +17,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-     List<String> notePad;
+    private List<String> notePad;
     private Context context;
     private RecyclerView recyclerView;
 
@@ -23,10 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Faça suas anotações!");
 
         notePad = new ArrayList<>();
 
         setaAdapterRecyclerView();
+
     }
 
     private void setaAdapterRecyclerView() {
@@ -39,9 +43,27 @@ public class MainActivity extends AppCompatActivity {
         notePad.add("Fazer o almoço");
         notePad.add("Arrumar o guarda-roupas");
 
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                RecyclerAdapter recyclerAdapter = new RecyclerAdapter(notePad, context);
+                notePad.remove(viewHolder.getAdapterPosition());
+                recyclerAdapter.notifyDataSetChanged();
+            }
+        };
+
+        new ItemTouchHelper(simpleCallback).attachToRecyclerView(recyclerView);
+
         recyclerView.setAdapter(recyclerAdapter);
-        recyclerAdapter.notifyDataSetChanged();
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAdapter.notifyDataSetChanged();
     }
+
+
 
 }
